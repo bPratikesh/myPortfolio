bf01d9c6b3b7c7c92a4d65a6560bfe57554cb269
Pratikesh <pratikeshb9@gmail.com>
2026-05-19 00:39:42 +0530

    worked on index/primary css file and on Navbar for web and mobile screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Experience } from "@/sections/Experience";
+import { Contact } from "@/sections/Contact";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Contact />
+      </main>
+    </div>
+  );
+}
 
 export default App;
